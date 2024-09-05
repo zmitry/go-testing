5a7f1eb4fb1473373b0f5e506b4aa8e55ebe842b
Zherebko Dmitry <kraken@live.ru>
2024-09-05 11:43:09 +0100

    fix

--- backend/a/main_test.go
+++ backend/a/main_test.go
@@ -24,3 +24,27 @@ func TestAdd(t *testing.T) {
 		})
 	}
 }
+
+
+func TestHello(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1+2=3", args{1, 2}, 3},
+		{"2+3=5", args{2, 3}, 5},
+		{"3+4=7", args{3, 4}, 7},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
