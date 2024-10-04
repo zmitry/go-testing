3c28fbcdc96684b28a60ec3936e1938f68467396
Dmitry Zherebko <kraken@live.ru>
2024-10-04 14:15:17 +0100

    Update main_test.go

--- backend/b/main_test.go
+++ backend/b/main_test.go
@@ -20,7 +20,7 @@ func TestAdd(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := a.Add(tt.args.a, tt.args.b); got != tt.want {
-				t.Errorf("Add() = %v, want %v", got, tt.want)
+				t.Errorf("Add() = %v, want %v", gt, tt.want)
 			}
 		})
 	}
